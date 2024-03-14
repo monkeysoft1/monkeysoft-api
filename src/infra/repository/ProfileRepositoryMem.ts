7c8ee5f23f3ea296124f16e57c03dfad2da5fcfb
Anderson Silva <andersonsstr@gmail.com>
2024-03-14 00:06:50 -0300

    fix: profile name attributes

--- src/infra/repository/ProfileRepositoryMem.ts
+++ src/infra/repository/ProfileRepositoryMem.ts
@@ -3,28 +3,28 @@ import IProfileRepository from "../../core/repository/IProfileRepository";
 import { GetAllDTO } from "./IGetAll";
 
 export default class ProfileRepositoryMem implements IProfileRepository {
-  feature: Profile[] = [];
+  profile: Profile[] = [];
   async getAll(input: GetAllDTO): Promise<any> {
     return {
-      list: this.feature,
-      total: this.feature.length,
-      total_page: this.feature.length,
+      list: this.profile,
+      total: this.profile.length,
+      total_page: this.profile.length,
     };
   }
 
   async getById(id: string): Promise<Profile | undefined> {
-    return this.feature.find((f) => f.id === id);
+    return this.profile.find((f) => f.id === id);
   }
 
   async getByName(name: string): Promise<Profile | undefined> {
-    return this.feature.find((f) => f.name === name);
+    return this.profile.find((f) => f.name === name);
   }
 
-  async save(feature: Profile): Promise<void> {
-    this.feature.push(feature);
+  async save(profile: Profile): Promise<void> {
+    this.profile.push(profile);
   }
 
-  async update(feature: Profile): Promise<void> {
-    this.feature = this.feature.map((i) => (i.id === feature.id ? feature : i));
+  async update(profile: Profile): Promise<void> {
+    this.profile = this.profile.map((i) => (i.id === profile.id ? profile : i));
   }
 }

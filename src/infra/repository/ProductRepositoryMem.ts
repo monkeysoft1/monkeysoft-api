824b5bbb071d45b8a67ce4b1925c569e75e55f1e
Anderson Silva <andersonsstr@gmail.com>
2024-03-23 14:45:50 -0300

    fix: removing profile from repo mem

--- src/infra/repository/ProductRepositoryMem.ts
+++ src/infra/repository/ProductRepositoryMem.ts
@@ -2,14 +2,8 @@ import Product from "../../core/entity/Product";
 import IProductRepository from "../../core/repository/IProductRepository";
 import { GetAllDTO } from "./IGetAll";
 
-interface ProfileProduct {
-  id_profile: string;
-  feature: Product;
-}
-
 export default class ProductRepositoryMem implements IProductRepository {
   feature: Product[] = [];
-  profileProduct: ProfileProduct[] = [];
 
   async getAll(input: GetAllDTO): Promise<any> {
     return {
@@ -17,10 +11,6 @@ export default class ProductRepositoryMem implements IProductRepository {
       total: this.feature.length,
       total_page: this.feature.length,
     };
-  }
-
-  async getByProfileId(id: string): Promise<Product[]> {
-    return this.profileProduct.filter((f) => f.id_profile === id).map((i) => i.feature);
   }
 
   async getById(id: string): Promise<Product | undefined> {

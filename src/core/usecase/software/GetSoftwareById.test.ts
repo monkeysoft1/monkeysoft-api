8d1c6f9fd7cb85e326e6ba0de4b448c3cd9f1acf
Anderson Silva <andersonsstr@gmail.com>
2024-03-29 16:45:00 -0300

    Feat: Inserting Lists on get software by ID

--- src/core/usecase/software/GetSoftwareById.test.ts
+++ src/core/usecase/software/GetSoftwareById.test.ts
@@ -1,10 +1,25 @@
+import FeatureRepositoryMem from "../../../infra/repository/FeatureRepositoryMem";
+import ProductRepositoryMem from "../../../infra/repository/ProductRepositoryMem";
+import ProfileRepositoryMem from "../../../infra/repository/ProfileRepositoryMem";
 import SoftwareRepositoryMem from "../../../infra/repository/SoftwareRepositoryMem";
+import CreateFeature from "../feature/CreateFeature";
+import CreateProduct from "../product/CreateProduct";
+import CreateProfile from "../profile/CreateProfile";
 import CreateSoftware from "./CreateSoftware";
 import GetSoftwareById from "./GetSoftwareById";
 
 test("should be search a software by id", async () => {
   const softwareRepository = new SoftwareRepositoryMem();
-  const getSoftwareById = new GetSoftwareById(softwareRepository);
+  const productRepository = new ProductRepositoryMem();
+  const featureRepository = new FeatureRepositoryMem();
+  const profileRepository = new ProfileRepositoryMem();
+
+  const getSoftwareById = new GetSoftwareById(
+    softwareRepository,
+    profileRepository,
+    featureRepository,
+    productRepository
+  );
 
   const createSoftware = new CreateSoftware(softwareRepository);
 
@@ -14,6 +29,33 @@ test("should be search a software by id", async () => {
     description: "API de mensagens",
   });
 
+  const createFeature = new CreateFeature(featureRepository, softwareRepository);
+
+  await createFeature.execute({
+    name: "send-messages",
+    id_software: software.id,
+    active: true,
+    description: "Envio de mensagens",
+  });
+
+  const createProfile = new CreateProfile(profileRepository, softwareRepository);
+
+  await createProfile.execute({
+    name: "Administrador",
+    id_software: software.id,
+    active: true,
+  });
+
+  const createProduct = new CreateProduct(productRepository, softwareRepository);
+
+  await createProduct.execute({
+    name: "Plano básico",
+    id_software: software.id,
+    active: true,
+    description: "Ideal para quem quer conhecer o produto",
+    price: 80.5,
+  });
+
   const softwares = await getSoftwareById.execute(software);
 
   expect(softwares).toHaveProperty(["id"]);
@@ -21,14 +63,30 @@ test("should be search a software by id", async () => {
 
 test("should be throw an error when id is empty", async () => {
   const softwareRepository = new SoftwareRepositoryMem();
-  const getSoftwareById = new GetSoftwareById(softwareRepository);
+  const productRepository = new ProductRepositoryMem();
+  const featureRepository = new FeatureRepositoryMem();
+  const profileRepository = new ProfileRepositoryMem();
+  const getSoftwareById = new GetSoftwareById(
+    softwareRepository,
+    profileRepository,
+    featureRepository,
+    productRepository
+  );
 
   await expect(getSoftwareById.execute({ id: "" })).rejects.toThrow();
 });
 
 test("should be throw an error when result is empty", async () => {
   const softwareRepository = new SoftwareRepositoryMem();
-  const getSoftwareById = new GetSoftwareById(softwareRepository);
+  const productRepository = new ProductRepositoryMem();
+  const featureRepository = new FeatureRepositoryMem();
+  const profileRepository = new ProfileRepositoryMem();
+  const getSoftwareById = new GetSoftwareById(
+    softwareRepository,
+    profileRepository,
+    featureRepository,
+    productRepository
+  );
 
   const createSoftware = new CreateSoftware(softwareRepository);
 
--- src/core/usecase/software/GetSoftwareById.ts
+++ src/core/usecase/software/GetSoftwareById.ts
@@ -1,8 +1,17 @@
+import { GetAllDTO } from "../../../infra/repository/IGetAll";
 import AppError from "../../entity/AppError";
+import IFeatureRepository from "../../repository/IFeatureRepository";
+import IProductRepository from "../../repository/IProductRepository";
+import IProfileRepository from "../../repository/IProfileRepository";
 import ISoftwareRepository from "../../repository/ISoftwareRepository";
 
 export default class GetSoftwareById {
-  constructor(readonly softwareRepository: ISoftwareRepository) {}
+  constructor(
+    readonly softwareRepository: ISoftwareRepository,
+    readonly profileRepository: IProfileRepository,
+    readonly featureRepository: IFeatureRepository,
+    readonly productRepository: IProductRepository
+  ) {}
 
   async execute(input: Input): Promise<Output> {
     if (!input.id) {
@@ -15,12 +24,55 @@ export default class GetSoftwareById {
       throw new AppError("Software não encontrado", 404);
     }
 
+    const getAllInput: GetAllDTO = {
+      id_software: software.id,
+      filters: { id_software: software.id },
+    };
+
+    const { list: profilesResult } = await this.profileRepository.getAll(getAllInput);
+    const { list: featuresResult } = await this.featureRepository.getAll(getAllInput);
+    const { list: productsResult } = await this.productRepository.getAll(getAllInput);
+
+    console.log(featuresResult);
+
+    const profiles: ProfileDTO[] = profilesResult.map((f: any) => ({
+      id: f.id,
+      name: f.name,
+      active: f.active,
+      created_on: f.created_on,
+      updated_on: f.updated_on,
+    }));
+
+    const features: FeatureDTO[] = featuresResult.map((f: any) => ({
+      id: f.id,
+      name: f.name,
+      url: f.url,
+      is_page: f.is_page,
+      description: f.description,
+      active: f.active,
+      created_on: f.created_on,
+      updated_on: f.updated_on,
+    }));
+
+    const products: ProductDTO[] = productsResult.map((f: any) => ({
+      id: f.id,
+      name: f.name,
+      description: f.description,
+      price: f.price,
+      active: f.active,
+      created_on: f.created_on,
+      updated_on: f.updated_on,
+    }));
+
     return {
       id: software.id,
       name: software.name,
       description: software.description,
       active: software.active,
       created_on: software.created_on,
+      profiles,
+      features,
+      products,
     };
   }
 }
@@ -35,4 +87,36 @@ interface Output {
   description: string;
   active: boolean;
   created_on?: Date;
+  features: any[];
+  profiles: any[];
+  products: any[];
+}
+
+interface ProfileDTO {
+  id: string;
+  name: string;
+  active: boolean;
+  created_on?: Date;
+  updated_on?: Date;
+}
+
+interface FeatureDTO {
+  id: string;
+  name: string;
+  url: string;
+  is_page: boolean;
+  description: string;
+  active: boolean;
+  created_on?: Date;
+  updated_on?: Date;
+}
+
+interface ProductDTO {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  active: boolean;
+  created_on?: Date;
+  updated_on?: Date;
 }
